7dd233884c88065411517de0df3858d4c22b714b
manfredo52 <46770277+manfredo52@users.noreply.github.com>
2021-03-28 22:46:37 -0400

    Code restructuring and option to hide the hud on game start.

---
+++ ImmersiveHud/Hud_Update_Patch.cs
@@ -0,0 +1,135 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using UnityEngine;
+using UnityEngine.UI;
+using HarmonyLib;
+
+namespace ImmersiveHud
+{
+    [HarmonyPatch(typeof(Hud), "Update")]
+    public class Hud_Update_Patch : ImmersiveHud
+    {
+        public static void setCompatibility(Transform hud)
+        {
+            // Compatibility check for Quick Slots mod.
+            if (hud.Find("QuickSlotsHotkeyBar"))
+            {
+                hasQuickSlotsEnabled = true;
+
+                if (!hasCanvasQuickSlots)
+                {
+                    hud.Find("QuickSlotsHotkeyBar").GetComponent<RectTransform>().gameObject.AddComponent<CanvasGroup>();
+                    hasCanvasQuickSlots = true;
+                }
+            }
+            else
+            {
+                hasQuickSlotsEnabled = false;
+            }
+        }
+
+        public static void updateHudElementTransparency(string hudElement, float targetAlpha, float time)
+        {
+            if (hudElement == "QuickSlotsHotkeyBar" && !hasQuickSlotsEnabled)
+                return;
+
+            Transform hudRoot = Hud.instance.transform.Find("hudroot");
+            float lerpedAlpha;
+
+            if (hudElement == "MiniMap")
+            {
+                lerpedAlpha = Mathf.Lerp(lastSetMapAlpha, targetAlpha, time / hudFadeDuration.Value);
+                hudRoot.Find(hudElement).GetComponent<Minimap>().m_mapImageSmall.CrossFadeAlpha(targetAlpha, time, false);
+                lastSetMapAlpha = hudRoot.Find(hudElement).GetComponent<CanvasGroup>().alpha;
+            }
+            else
+            {
+                lerpedAlpha = Mathf.Lerp(lastSetAlpha, targetAlpha, time / hudFadeDuration.Value);
+                lastSetAlpha = hudRoot.Find(hudElement).GetComponent<CanvasGroup>().alpha;
+            }
+
+            hudRoot.Find(hudElement).GetComponent<CanvasGroup>().alpha = lerpedAlpha;
+        }
+
+        public static bool checkHudLerpDuration(float timeElapsed)
+        {
+            if (timeElapsed >= hudFadeDuration.Value)
+                return true;
+            else
+                return false;
+        }
+
+        public static void getPlayerData(Transform hud)
+        {
+            Minimap playerMap = hud.Find("MiniMap").GetComponent<Minimap>();
+            bool prevState = isMiniMapActive;
+
+            isMiniMapActive = playerMap.m_smallRoot.activeSelf;
+
+            // Reset timer when changing map modes.
+            if (prevState != isMiniMapActive)
+                timeMapFade = 0;
+        }
+
+        public static void setValuesBasedOnHud(bool pressedKey)
+        {
+            if (pressedKey)
+            {
+                hudHidden = !hudHidden;
+                timeFade = 0;
+                timeMapFade = 0;
+            }
+
+            if (hudHidden)
+            {
+                targetAlpha = 0;
+
+                if (isMiniMapActive)
+                    targetMapAlpha = 0;
+                else
+                    targetMapAlpha = 1;
+            }
+            else
+            {
+                targetAlpha = 1;
+                targetMapAlpha = 1;
+            }
+        }
+
+        private static void Postfix(Hud __instance)
+        {
+            Player localPlayer = Player.m_localPlayer;
+
+            if (!isEnabled.Value || !localPlayer)
+                return;
+
+            Transform hudRoot = __instance.transform.Find("hudroot");
+
+            getPlayerData(hudRoot);
+            setCompatibility(hudRoot);
+            setValuesBasedOnHud(Input.GetKeyDown(hideHudKey.Value.MainKey));
+
+            // Hud elements
+            targetAlphaHasBeenReached = checkHudLerpDuration(timeFade);
+
+            if (!targetAlphaHasBeenReached)
+            {
+                timeFade += Time.deltaTime;
+
+                foreach (string hudElement in hudElements)
+                    updateHudElementTransparency(hudElement, targetAlpha, timeFade);
+
+                updateHudElementTransparency("QuickSlotsHotkeyBar", targetAlpha, timeFade);
+            }
+
+            // Minimap
+            targetMapAlphaHasBeenReached = checkHudLerpDuration(timeMapFade);
+
+            if (!targetMapAlphaHasBeenReached)
+            {
+                timeMapFade += Time.deltaTime;
+                updateHudElementTransparency("MiniMap", targetMapAlpha, timeMapFade);
+            }
+        }
+    }
+}
--- ImmersiveHud/ImmersiveHud.cs
+++ ImmersiveHud/ImmersiveHud.cs
@@ -6,7 +6,7 @@ using HarmonyLib;
 
 namespace ImmersiveHud
 {
-    [BepInPlugin("manfredo52.ImmersiveHud", "Immersive Hud", "1.0.0")]
+    [BepInPlugin("manfredo52.ImmersiveHud", "Immersive Hud", "1.0.1")]
     [BepInProcess("valheim.exe")]
     public class ImmersiveHud : BaseUnityPlugin
     {
@@ -16,18 +16,21 @@ namespace ImmersiveHud
 
         // Main Settings
         public static ConfigEntry<KeyboardShortcut> hideHudKey;
+        public static ConfigEntry<bool> hudHiddenOnStart;
         public static ConfigEntry<float> hudFadeDuration;
 
         public static bool hudHidden;
+        public static bool isMiniMapActive;
+
         public static float timeFade = 0;
         public static float targetAlpha;
         public static float lastSetAlpha;
-        public static bool targetAlphaHasBeenReached;
 
-        public static bool isMiniMapActive;
-        public static float targetMapAlpha;
-        public static float lastSetMapAlpha;
         public static float timeMapFade = 0;
+        public static float targetMapAlpha;
+        public static float lastSetMapAlpha;  
+
+        public static bool targetAlphaHasBeenReached;
         public static bool targetMapAlphaHasBeenReached;
 
         // Compatibility
@@ -58,7 +61,8 @@ namespace ImmersiveHud
 
             // Main Settings
             hideHudKey = Config.Bind<KeyboardShortcut>("- Main Settings -", "hideHudKey", new KeyboardShortcut(KeyCode.H), "Keyboard shortcut or mouse button to hide the hud.");
-            hudFadeDuration = Config.Bind<float>("- Main Settings -", "hudFadeDuration", 1, "hud fade duration.");
+            hudHiddenOnStart = Config.Bind<bool>("- Main Settings -", "hudHiddenOnStart", false, "Hide the hud when the game is started.");
+            hudFadeDuration = Config.Bind<float>("- Main Settings -", "hudFadeDuration", 1, "How quickly the hud fades in or out.");       
 
             DoPatching();
         }
@@ -79,133 +83,8 @@ namespace ImmersiveHud
                 // Add CanvasGroup to each hud element on awake.
                 foreach (string hudElement in hudElements)
                     hudRoot.Find(hudElement).GetComponent<RectTransform>().gameObject.AddComponent<CanvasGroup>();
-            }
-        }
 
-        [HarmonyPatch(typeof(Hud), "Update")]
-        public class Hud_Update_Patch
-        {
-            public static void setCompatibility(Transform hud)
-            {
-                // Compatibility check for Quick Slots mod.
-                if (hud.Find("QuickSlotsHotkeyBar"))
-                {
-                    hasQuickSlotsEnabled = true;
-
-                    if (!hasCanvasQuickSlots)
-                    {
-                        hud.Find("QuickSlotsHotkeyBar").GetComponent<RectTransform>().gameObject.AddComponent<CanvasGroup>();
-                        hasCanvasQuickSlots = true;
-                    }
-                } 
-                else
-                {
-                    hasQuickSlotsEnabled = false;
-                }
-            }
-
-            public static void updateHudElementTransparency(string hudElement, float targetAlpha, float time)
-            {
-                if (hudElement == "QuickSlotsHotkeyBar" && !hasQuickSlotsEnabled)
-                    return;
-
-                Transform hudRoot = Hud.instance.transform.Find("hudroot");
-                float lerpedAlpha;
-
-                if (hudElement == "MiniMap")
-                {
-                    lerpedAlpha = Mathf.Lerp(lastSetMapAlpha, targetAlpha, time / hudFadeDuration.Value);
-                    hudRoot.Find(hudElement).GetComponent<Minimap>().m_mapImageSmall.CrossFadeAlpha(targetAlpha, time, false);
-                    lastSetMapAlpha = hudRoot.Find(hudElement).GetComponent<CanvasGroup>().alpha;
-                } 
-                else
-                {
-                    lerpedAlpha = Mathf.Lerp(lastSetAlpha, targetAlpha, time / hudFadeDuration.Value);            
-                    lastSetAlpha = hudRoot.Find(hudElement).GetComponent<CanvasGroup>().alpha;
-                }
-
-                hudRoot.Find(hudElement).GetComponent<CanvasGroup>().alpha = lerpedAlpha;
-            }
-
-            public static bool checkHudLerpDuration(float timeElapsed)
-            {
-                if (timeElapsed >= hudFadeDuration.Value)
-                    return true;
-                else
-                    return false;
-            }
-
-            public static void getPlayerData(Transform hud)
-            {
-                Minimap playerMap = hud.Find("MiniMap").GetComponent<Minimap>();
-                bool prevState = isMiniMapActive;
-
-                isMiniMapActive = playerMap.m_smallRoot.activeSelf;
-
-                // Reset timer when changing map modes.
-                if (prevState != isMiniMapActive)
-                    timeMapFade = 0;
-            }
-
-            public static void setValuesBasedOnHud(bool pressedKey)
-            {
-                if (pressedKey)
-                {
-                    hudHidden = !hudHidden;
-                    timeFade = 0;
-                    timeMapFade = 0;
-                }
-
-                if (hudHidden)
-                {
-                    targetAlpha = 0;
-
-                    if (isMiniMapActive)
-                        targetMapAlpha = 0;
-                    else
-                        targetMapAlpha = 1;
-                } 
-                else
-                {
-                    targetAlpha = 1;
-                    targetMapAlpha = 1;
-                }
-            }
-
-            private static void Postfix(Hud __instance)
-            {
-                Player localPlayer = Player.m_localPlayer;
-
-                if (!isEnabled.Value || !localPlayer)
-                    return;
-
-                Transform hudRoot = __instance.transform.Find("hudroot");
-
-                getPlayerData(hudRoot);
-                setCompatibility(hudRoot);
-                setValuesBasedOnHud(Input.GetKeyDown(hideHudKey.Value.MainKey));           
-
-                // Hud elements
-                targetAlphaHasBeenReached = checkHudLerpDuration(timeFade);
-
-                if (!targetAlphaHasBeenReached)
-                {
-                    timeFade += Time.deltaTime;
-
-                    foreach (string hudElement in hudElements)
-                        updateHudElementTransparency(hudElement, targetAlpha, timeFade);
-
-                    updateHudElementTransparency("QuickSlotsHotkeyBar", targetAlpha, timeFade);
-                }
-
-                // Minimap
-                targetMapAlphaHasBeenReached = checkHudLerpDuration(timeMapFade);
-
-                if (!targetMapAlphaHasBeenReached)
-                {
-                    timeMapFade += Time.deltaTime;
-                    updateHudElementTransparency("MiniMap", targetMapAlpha, timeMapFade);
-                }     
+                hudHidden = hudHiddenOnStart.Value;
             }
         }
     }
