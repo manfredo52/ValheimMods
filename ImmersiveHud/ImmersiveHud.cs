b6bcaa7030bb5b819928d78a606858711700797f
manfredo52 <manfredisportillo@gmail.com>
2021-09-21 11:32:29 -0400

    iHud Updated for H&H

--- ImmersiveHud/ImmersiveHud.cs
+++ ImmersiveHud/ImmersiveHud.cs
@@ -9,7 +9,7 @@ using HarmonyLib;
 
 namespace ImmersiveHud
 {
-    [BepInPlugin("manfredo52.ImmersiveHud", "Immersive Hud", "1.2.6")]
+    [BepInPlugin("manfredo52.ImmersiveHud", "Immersive Hud", "1.2.7")]
     [BepInProcess("valheim.exe")]
     public class ImmersiveHud : BaseUnityPlugin
     {
